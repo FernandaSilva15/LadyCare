276b04c54161b5c81b5a80ff7df001a31136cf87
izaholands <2022326925@ifam.edu.br>
2024-04-11 17:27:28 -0400

    Tela Calendario e relatorio

---
+++ app/src/main/java/com/example/telas_lady/tela_relatorio.java
@@ -0,0 +1,14 @@
+package com.example.telas_lady;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tela_relatorio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_relatorio);
+    }
+}
